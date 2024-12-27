7c3c4b4773a2d7892f1935e6dbf42028b846cb50
Iktisad <iktisad.rashid@hotmail.com>
2024-12-26 22:35:41 -0500

    folder path fixed for images

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  assetsInclude: ['**/*.JPG'], // Add this line
   base: "./",
   resolve: {
     alias: {
