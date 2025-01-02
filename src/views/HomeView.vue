8490e18b9e2ada7a505411a560c55d4efd37aa4b
SayeedSanjana <sanjanasayeed68@gmail.com>
2025-01-01 22:11:51 -0500

    working fine

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -108,7 +108,7 @@
                   "Bridging Distances, Celebrating Culture"
                 </p>
               </blockquote>
-              <div class="flex mt-4">
+              <div class="flex pt-4 lg:pt-14">
                 <button
                   @click="join"
                   class="mr-2 text-base border-2 border-yellow-400 px-4 py-2 mt-4 shadow-md bg-gradient-to-r from-yellow-400 to-yellow-500 text-white font-semibold rounded-lg transition duration-300 ease-in-out lg:hover:bg-gradient-to-r lg:hover:from-gray-50 lg:hover:to-white lg:hover:border-honey-gold lg:hover:text-honey-gold"
