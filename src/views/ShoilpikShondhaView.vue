245ed0dab5837f35c5f85263a92b5bb7d58fe6d2
SayeedSanjana <sanjanasayeed68@gmail.com>
2025-07-17 13:58:35 -0400

    ok

--- src/views/ShoilpikShondhaView.vue
+++ src/views/ShoilpikShondhaView.vue
@@ -142,17 +142,15 @@
     <div
       class="min-h-screen flex flex-col justify-center items-center bg-white py-16"
     >
-      <h2
-        class="text-4xl md:text-6xl mb-20 md:mb-28 text-teal-900 font-cormorant text-center"
-      >
+      <h2 class="text-4xl md:text-6xl text-teal-900 font-cormorant text-center">
         About the Event
       </h2>
-      <p class="text-gray-600 mt-4 mb-4 text-center">
+      <p class="text-gray-600 mt-4 mb-20 md:mb-28 text-center">
         We’re excited to take part in Shoilpik Sondhya, a vibrant celebration of
         Bengali culture, music, and theatre, bringing together artists and
         audiences from across Canada. Join us for an evening filled with rhythm,
         stories, and shared heritage
-      </p>
+      </p> 
 
       <div
         class="relative flex flex-col lg:flex-row gap-10 lg:items-center sm:items-stretch"
